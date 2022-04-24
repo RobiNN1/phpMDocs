ba7e6c6f53a8daae4357e66a1352222198954957
RobiNN <kelcakrobo@gmail.com>
2022-04-25 00:04:53 +0200

    Fixes

--- src/Controllers/HomepageController.php
+++ src/Controllers/HomepageController.php
@@ -19,6 +19,7 @@ class HomepageController extends Documentation {
      * @return void
      */
     public function show(): void {
+        // this is intentionally in own file, so the dev can easily add own code to the homepage
         echo $this->tpl('home', [
             'categories' => $this->cacheData('homepage_categories', $this->getPages('', true)),
         ]);
