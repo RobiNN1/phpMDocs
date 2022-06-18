5addc52e78240edb92c536d4794564a9024b1e3b
RobiNN <kelcakrobo@gmail.com>
2022-06-18 16:09:36 +0200

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,9 +5,8 @@ let search_field = document.getElementById('searchdocs');
 let search_result = document.getElementById('search_result');
 
 if (search_field) {
-    let no_items = `<li>
-    <span class="block text-gray-500 py-3 px-2.5 truncate">Enter a search term to find results.</span>
-    </li>`;
+    let no_items = `<li><span class="block text-gray-500 py-3 px-2.5 truncate">Enter a search term to find results.</span></li>`;
+
     search_result.innerHTML = no_items;
 
     search_field.addEventListener('keyup', () => {
@@ -34,14 +33,16 @@ const render_results = (resp) => {
     let result = '';
 
     if (!resp.status) {
-        Array.prototype.forEach.call(resp, (data) => {
+        Array.prototype.forEach.call(resp, data => {
             if (data) {
                 if (data.page !== data.title) {
                     result += `<li><a class="block hover:bg-gray-100 py-1.5 px-2.5 text-gray-800 hover:text-gray-700 truncate" href="${data.link}">
-                                    <span class="font-normal opacity-75">${data.page}</span><br> ${data.title}
-                                </a></li>`;
+                                   <span class="font-normal opacity-75">${data.page}</span><br> ${data.title}
+                               </a></li>`;
                 } else {
-                    result += `<li><a class="block hover:bg-gray-100 py-1.5 px-2.5 text-gray-800 hover:text-gray-700 truncate" href="${data.link}">${data.title}</a></li>`;
+                    result += `<li>
+                                   <a class="block hover:bg-gray-100 py-1.5 px-2.5 text-gray-800 hover:text-gray-700 truncate" href="${data.link}">${data.title}</a>
+                               </li>`;
                 }
             }
         });
